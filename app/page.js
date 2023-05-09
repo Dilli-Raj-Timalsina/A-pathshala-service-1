a6e76842ea9433ef4d03a3092462b3105bbeb710
the-gaurav-bhatt <bhattgaurav654@gmail.com>
2023-05-09 10:07:29 +0545

    Hero Section Added

--- app/page.js
+++ app/page.js
@@ -1,11 +1,11 @@
 import PopularCourses from './components/courses/PopularCourses/PopularCourses';
 import PopularTeachers from './components/courses/PopularTeachers/PopularTeachers';
-// import Hero from './components/hero/HeroSection';
+import Hero from './components/hero/HeroSection';
 import React from 'react';
 export default function Home() {
   return (
     <div className="bg-content-white-gradient">
-      {/* <Hero /> */}
+      <Hero />
 
       <div className="py-2 mb-4">
         <PopularTeachers />

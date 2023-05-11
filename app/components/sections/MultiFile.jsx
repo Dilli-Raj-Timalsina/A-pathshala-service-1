4ba95c0a438f05be0ff581aed1ff576280e0c462
the-gaurav-bhatt <bhattgaurav654@gmail.com>
2023-05-11 22:45:35 +0545

    minor bug fixed

--- app/components/sections/MultiFile.jsx
+++ app/components/sections/MultiFile.jsx
@@ -1,7 +1,7 @@
 'use client';
 import React, { useState } from 'react';
 import axios from 'axios';
-
+//// ONLY FOR TESTING PURPOSE
 const VideoUploadForm = () => {
   const [uploadProgress, setUploadProgress] = useState(0);
   const [uploadPaused, setUploadPaused] = useState(false);
